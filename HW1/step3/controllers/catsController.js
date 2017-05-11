0b01dd36f7a5d66edda562347a6e44affe5ebac4
Siva Rallabhandi <Siva_Rallabhandi@epam.com>
2017-05-11 11:31:06 +0530

    completed step3 in HW1

--- HW1/step3/controllers/catsController.js
+++ HW1/step3/controllers/catsController.js
@@ -7,12 +7,29 @@
         vm.selectedCat = null;
 
         vm.cats = [{
+            id: 1,
             name: 'cat 1',
             imgSrc: './assets/images/cat1.jpg',
             clickCount: 0
         }, {
+            id: 2,
             name: 'cat 2',
             imgSrc: './assets/images/cat2.jpg',
+            clickCount: 0
+        }, {
+            id: 3,
+            name: 'cat 3',
+            imgSrc: './assets/images/cat3.jpg',
+            clickCount: 0
+        }, {
+            id: 4,
+            name: 'cat 4',
+            imgSrc: './assets/images/cat4.jpg',
+            clickCount: 0
+        }, {
+            id: 5,
+            name: 'cat 5',
+            imgSrc: './assets/images/cat5.jpg',
             clickCount: 0
         }];
 
